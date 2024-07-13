2d24fd9d422f110184cccdd306347fc53c98461f
Ahmad Badawi Kasyfur Rahman <ahmad.badawi.k@mail.ugm.ac.id>
2024-07-13 22:50:15 +0700

    learn interface and type assertion

--- tests/interface.test.ts
+++ tests/interface.test.ts
@@ -1,4 +1,7 @@
 import { Seller } from "../src/seller"
+import { Employee, Manager } from "../src/employee";
+import { Person } from "../src/person"
+
 describe("Interface", function(){
     it("Should supported in ts", function(){
         const seller: Seller = {
@@ -48,6 +51,71 @@ describe("Interface", function(){
 
         expect(dictionary["name"]).toBe("Aemond");
         expect(dictionary["address"]).toBe("Aegonfort");
+    })
+
+    it("should support extends interface", function (){
+
+        const employee: Employee = {
+            id:"1",
+            name:"Daemon",
+            division:"Audit"
+        }
+
+        console.info(employee)
+
+        const manager: Manager = {
+            id:"2",
+            name:"Viserys",
+            division: "Audit",
+            numberOfEmployees:30
+
+        }
+
+        console.info(manager)
+    });
+
+    it("should support function in interface", function (){
+        
+
+        const person: Person = {
+            name: "Aemond",
+            sayHello: function (name: string): string {
+                return `Hello ${name}, my name is ${this.name}`;
+            }
+        }
+
+        console.info(person.sayHello("Targaryen"));
+
+    });
+
+    it("support intersection types", function(){
+
+        interface HasName {
+            name: string
+        }
+
+        interface HasId {
+            id: string
+        }
+
+        type Domain = HasId & HasName;
+
+        const domain: Domain = {
+            id:"1",
+            name: "Daemon"
+        };
+
+        console.info(domain);
+    });
+
+    it("should support type assertion", function (){
+        const person: any = {
+            name:"Daemon",
+            age: 55
+        }
+        const person2 : Person = person as Person;
+        console.info(person2)
+
     })
 
 });
